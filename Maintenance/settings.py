3f53560ac87478bb47f102057b1431aff9d8285a
ErickLiang <290727629@qq.com>
2020-03-13 16:13:03 +0800

    基础配置修改
    修改了语言，时区等。

--- Maintenance/settings.py
+++ Maintenance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-^ker&&g(kly@nsens*dn0gyp&bf33zkjwse=)aq!$87&#$kv$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "index",
 ]
 
 MIDDLEWARE = [
@@ -103,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
