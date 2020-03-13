03c62530f311c4bed88807b1c23c0d703c5d60e8
ErickLiang <290727629@qq.com>
2020-03-13 17:47:57 +0800

    add index demo

--- index/models.py
+++ index/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, verbose_name='项目名称')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name="开始时间")
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    ip = models.GenericIPAddressField(verbose_name='服务器ip')
+    password = models.CharField(max_length=256, verbose_name="服务器密码")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name="所属项目")
+
+    def __str__(self):
+        return self.project
--- index/views.py
+++ index/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Server, Project
 # Create your views here.
+
+def index(request):
+    all_project = Project.objects.all()
+
+    return render(request, 'index.html', locals())
